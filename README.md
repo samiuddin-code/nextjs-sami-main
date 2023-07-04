15030664c1b93f1d2c2775361a3612e66ebf997e
samiuddin-code <samiuddinbirgoshi@gmail.com>
2023-07-04 09:57:03 +0400

    Commits

---
+++ README.md
@@ -0,0 +1,14 @@
+This  project is intented for your pratice purpose and the things and funtions used is next 13.4 
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+```
+@auther sami uddin
---
+++ src/app/page.jsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "public/hero.png";
+import Button from "@/components/Button/Button";
+import React from "react";
+
+export const metadata = {
+  title: "Lama Dev Contact Information",
+  description: "This is Contact Page",
+};
+
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <Button url="/portfolio" text="See Our Works"/>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
+    </div>
+  
+  );
+  
+  }
+ 
+const Contact = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src="/contact.png"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button url="#" text="Send"/>
+        </form>
+      </div>
+    </div>
+  );
+};
+
